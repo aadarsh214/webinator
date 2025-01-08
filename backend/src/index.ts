969c9f55781b4978a1acc5595d6eabc8a07e77cd
aadarsh214 <aadarshgupta7828@gmail.com>
2025-01-08 23:19:42 +0530

    added Google Generative AI integration and update environment configuration

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,12 @@
 require("dotenv").config();
+console.log(process.env.GEMINI_API_KEY);
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+
+
+async function main() {
+    const genAI = new GoogleGenerativeAI("GEMINI_API_KEY");
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+    const prompt = "2+2";
+    const result = await model.generateContent(prompt);
+    console.log(result.response.text());
+}
